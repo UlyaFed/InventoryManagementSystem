b5b33a162a05f0a95c36a28eeabf9108a080037d
brittaktb <brittaktb@web.de>
2025-04-02 11:31:47 +0200

    main_file

---
+++ inventory/main.py
@@ -0,0 +1,25 @@
+from product import Product
+from inventory_manager import InventoryManager
+
+
+def main():
+    
+    manager = InventoryManager()
+    product1 = Product("Laptop", 1200, 1)
+    product2= Product("Mouse", 25, 5)
+    product3 = Product("Keyboard", 19.99, 3)
+    manager.add_products(product1)
+    manager.add_products(product2)
+    manager.add_products(product3)
+    manager.update_quantity("Mouse", 10)
+    manager.get_inventory_info()
+    manager.remove_products("Mouse")
+    manager.get_total_inventory_value()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
